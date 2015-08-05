dde8b1c11df20f07dce1eef9bf70318346363b91
Spencer <spencer@branch2.com>
2015-08-05 00:38:44 -0500

    Video section added

---
+++ js/video.js
@@ -0,0 +1,17 @@
+//
+//(function() {
+//    'use strict';
+//var video1 = document.getElementById("video1");
+//video1.autoplay = true;
+//video1.load();
+//var video2 = document.getElementById("video2");
+//video2.autoplay = true;
+//video2.load();
+//var video3 = document.getElementById("video3");
+//video3.autoplay = true;
+//video3.load();
+//var video4 = document.getElementById("video4");
+//video4.autoplay = true;
+//video4.load();
+//
+// })();
